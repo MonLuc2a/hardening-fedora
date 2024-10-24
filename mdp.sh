7da6a18fe53a3d6fc73343994e32c25fc4c4abe3
Lucas Monribot <98400136+MonLuc2a@users.noreply.github.com>
2024-10-24 16:18:19 +0200

    Update mdp.sh

--- mdp.sh
+++ mdp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Modifier /etc/login.defs
+sed -i 's/^UMASK.*/UMASK   027/' /etc/login.defs
 sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS   30/' /etc/login.defs
 sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   12/' /etc/login.defs
 sed -i 's/^PASS_MIN_LEN.*/PASS_MIN_LEN    12/' /etc/login.defs
